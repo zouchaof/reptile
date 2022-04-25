8ce572cb6ee3c66d3b371b9f012a300f947d7bbb
zouchao <zouchao0000@163.com>
2022-04-25 22:29:57 +0800

    1
    Signed-off-by: zouchao <zouchao0000@163.com>

--- reptile-web/src/main/java/com/reptile/web/start/BootStrap.java
+++ reptile-web/src/main/java/com/reptile/web/start/BootStrap.java
@@ -5,12 +5,14 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @Slf4j
 @SpringBootApplication(scanBasePackages={"com.reptile"})
 //@MapperScan("com.reptile.mapper.dao")
-public class BootStrap implements CommandLineRunner {
+public class BootStrap extends SpringBootServletInitializer implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(BootStrap.class, args);
@@ -23,4 +25,8 @@ public class BootStrap implements CommandLineRunner {
         System.setProperty("sun.net.client.defaultReadTimeout", "30000");
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BootStrap.class);
+    }
 }
